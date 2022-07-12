3218d07faeac91d9636e4cd90afbb3b0efd87efa
Tresor-ilunga <19im065@esisalama.org>
2022-07-12 22:49:44 +0200

    Mise en place du controller

---
+++ src/include.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Les fonctions appelée par le controlloer
+ */
+require "functions.php";
+
+define("SRC", dirname(__FILE__));
+define("ROOT", dirname(SRC));
+define("SEPARATOR", DIRECTORY_SEPARATOR);
+define("VIEWS", ROOT.SEPARATOR."views");
+define("MODEL", ROOT.SEPARATOR."model");
--- src/index.php
+++ src/index.php
@@ -1,2 +1,24 @@
 <?php
+require "include.php";
+$url=  trim( $_SERVER['PATH_INFO'], '/');
+$url = explode('/',$url);
+$route = array(
+    "accueil",
+    "contact"
+);
 
+//print_r($url);
+
+$action = $url[0];
+
+//Controller
+if(!in_array($action, $route))
+{
+    echo 'error';
+}
+else
+{
+    //echo "Bienvenue sur la page " .$action;
+    $function = "display".ucwords($action)."()";
+    echo $function;
+}
