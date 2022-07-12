ee121c0ad1348767be17c4f9063da41c243daf0c
Tresor-ilunga <19im065@esisalama.org>
2022-07-12 23:23:04 +0200

    Mise en place de la Vue

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,11 @@
 <?php
+
+function displayAccueil()
+{
+    return "<h1>Bienvenu sur la page d'accueil</h1>";
+}
+
+function displayContact()
+{
+    return "<h1>Bienvenu sur la page de contact</h1>";
+}
--- src/index.php
+++ src/index.php
@@ -14,11 +14,14 @@ $action = $url[0];
 //Controller
 if(!in_array($action, $route))
 {
-    echo 'error';
+    $title = "Page Error";
+    $content = "URL introuvable";
 }
 else
 {
     //echo "Bienvenue sur la page " .$action;
-    $function = "display".ucwords($action)."()";
-    echo $function;
+    $function = "display".ucwords($action);
+    $title = "Page ".$action;
+    $content = $function();
 }
+require_once VIEWS.SEPARATOR."templates".SEPARATOR."default.php";
